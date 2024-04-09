6b592651bb4598524772d3362f48a6c4bbc592e8
caua467 <cauakellysilvaesilva@gmail.com>
2024-04-09 14:51:20 -0300

    home

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, RouterModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
