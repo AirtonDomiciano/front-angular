6b90152a99622b02da4fca72e29e26294fcd76d5
PedroPetrikoski <pedro.ribeiro@xpert.com.br>
2024-04-26 15:02:45 -0300

    criando popup perfil topnav

--- src/app/core/components/top-nav/model/top-nav.component.ts
+++ src/app/core/components/top-nav/model/top-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-nav',
@@ -6,9 +7,26 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./top-nav.component.scss'],
 })
 export class TopNavComponent {
+  public popUpAtivo: boolean = false;
+
+  constructor(private router: Router) {}
+
   @Output() emitterSignOut: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  editarPerfil() {
+    this.router.navigate([`edit-user/`]);
+  }
+
+  minhasCompras() {
+    this.router.navigate([`shopping`]);
+  }
+
+  popUp() {
+    this.popUpAtivo = !this.popUpAtivo;
+  }
 
   signOut() {
     this.emitterSignOut.emit(false);
+    this.router.navigate([`login`]);
   }
 }
