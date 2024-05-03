ec13eee76ed11a4936848eec943560160ac11a81
PedroPetrikoski <pedro.ribeiro@xpert.com.br>
2024-05-03 16:32:14 -0300

    criando input number

--- src/app/shared/components/input-number/input-number.component.ts
+++ src/app/shared/components/input-number/input-number.component.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import { Component, Input, OnInit } from '@angular/core';
+=======
+import { Component, Input } from '@angular/core';
+>>>>>>> e579271 (criando input number)
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,15 +10,22 @@ import { FormGroup } from '@angular/forms';
   templateUrl: './input-number.component.html',
   styleUrls: ['./input-number.component.scss'],
 })
+<<<<<<< HEAD
 export class InputNumberComponent implements OnInit {
+=======
+export class InputNumberComponent {
+>>>>>>> e579271 (criando input number)
   @Input() form!: FormGroup;
   @Input() type: string = 'number';
   @Input() class = 'form-control rounded-pill mt-2';
   @Input() id = '';
   @Input() frmName = '';
   @Input() placeholder = '';
+<<<<<<< HEAD
 
   constructor() {}
 
   ngOnInit(): void {}
+=======
+>>>>>>> e579271 (criando input number)
 }
