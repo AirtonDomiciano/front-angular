d5184a2473ccc8e02a3b2ffbb3502f2e1c5a84fd
PedroPetrikoski <pedro.ribeiro@xpert.com.br>
2024-05-07 14:45:36 -0300

    criando input number

--- src/app/shared/components/input-number/input-number.component.ts
+++ src/app/shared/components/input-number/input-number.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,11 +6,15 @@ import { FormGroup } from '@angular/forms';
   templateUrl: './input-number.component.html',
   styleUrls: ['./input-number.component.scss'],
 })
-export class InputNumberComponent {
+export class InputNumberComponent implements OnInit {
   @Input() form!: FormGroup;
+  @Input() type: string = 'number';
   @Input() class = 'form-control rounded-pill mt-2';
   @Input() id = '';
   @Input() frmName = '';
   @Input() placeholder = '';
-  @Input() min = '0';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
