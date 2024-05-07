5f4c90d365ffdd28d8f67b671fa4c859022833ca
PedroPetrikoski <pedro.ribeiro@xpert.com.br>
2024-05-07 16:07:47 -0300

    subindo  rebase

--- src/app/shared/components/input-number/input-number.component.ts
+++ src/app/shared/components/input-number/input-number.component.ts
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Component, Input, OnInit } from '@angular/core';
-=======
-import { Component, Input } from '@angular/core';
->>>>>>> e579271 (criando input number)
-=======
-import { Component, Input, OnInit } from '@angular/core';
->>>>>>> 5572933 (criando input number)
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -14,36 +6,16 @@ import { FormGroup } from '@angular/forms';
   templateUrl: './input-number.component.html',
   styleUrls: ['./input-number.component.scss'],
 })
-<<<<<<< HEAD
-<<<<<<< HEAD
 export class InputNumberComponent implements OnInit {
-=======
-export class InputNumberComponent {
->>>>>>> e579271 (criando input number)
-=======
-export class InputNumberComponent implements OnInit {
->>>>>>> 5572933 (criando input number)
   @Input() form!: FormGroup;
   @Input() type: string = 'number';
   @Input() class = 'form-control rounded-pill mt-2';
   @Input() id = '';
   @Input() frmName = '';
   @Input() placeholder = '';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 5572933 (criando input number)
+  @Input() min = '0';
 
   constructor() {}
 
   ngOnInit(): void {}
-<<<<<<< HEAD
-=======
->>>>>>> e579271 (criando input number)
-=======
-  @Input() min = '0';
->>>>>>> 0b267ab (criando input number)
-=======
->>>>>>> 5572933 (criando input number)
 }
