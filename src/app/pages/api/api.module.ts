83c21b773ad6031be845150af63c1a0f14ee60f2
PedroPetrikoski <pedro.ribeiro@xpert.com.br>
2024-05-08 15:28:18 -0300

    criando routing module api

--- src/app/pages/api/api.module.ts
+++ src/app/pages/api/api.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { ApiComponent } from '../api/api.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ApiRoutingModule } from './api-routing.module';
 
 @NgModule({
   declarations: [ApiComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, ApiRoutingModule],
   exports: [ApiComponent],
 })
 export class ApiModule {}
