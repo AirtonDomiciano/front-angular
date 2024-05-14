9344f0ac5489bb4ea458804e8fa3410ca39ddbd1
PedroPetrikoski <pedro.ribeiro@xpert.com.br>
2024-05-14 15:26:41 -0300

    subindo rotas com id

--- src/app/pages/usuario/usuario-routing.module.ts
+++ src/app/pages/usuario/usuario-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsuarioComponent } from './usuario.component';
+import { ActivatedRoute } from '@angular/router';
 
-const routes: Routes = [{ path: '', component: UsuarioComponent }];
+const routes: Routes = [
+  { path: '', component: UsuarioComponent },
+  { path: ':id', component: UsuarioComponent },
+];
+
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class UsuarioRoutingModule {}
+export class UsuarioRoutingModule {
+  public id = Number(this.route.snapshot.paramMap.get('id'));
+
+  constructor(private route: ActivatedRoute) {}
+}
