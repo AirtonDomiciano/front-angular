2ea2d3a32ffe6286ba1e7955b332ff31708009c7
Yuri Schenkel Dalle Teze <106547579+YuriSchenkel@users.noreply.github.com>
2024-05-16 15:18:37 -0300

    subindo tabelas

--- src/app/pages/usuarios/usuarios.module.ts
+++ src/app/pages/usuarios/usuarios.module.ts
@@ -3,10 +3,11 @@ import { UsuariosComponent } from './usuarios.component';
 import { CepPipeModule } from 'src/app/shared/pipes/cep-pipe.module';
 import { CommonModule } from '@angular/common';
 import { UsuariosRoutingModule } from './usuarios-routing.module';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [UsuariosComponent],
-  imports: [CommonModule, CepPipeModule, UsuariosRoutingModule],
+  imports: [CommonModule, CepPipeModule, UsuariosRoutingModule, TableModule],
   exports: [UsuariosComponent],
 })
 export class UsuariosModule {}
