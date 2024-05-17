afeebaa5eb93bc8c9a757560f92ce397f12a5700
PedroPetrikoski <pedro.ribeiro@xpert.com.br>
2024-05-17 14:33:31 -0300

    subindo ajustes no core

--- src/app/core/components/side-nav/model/routes.model.ts
+++ src/app/core/components/side-nav/model/routes.model.ts
@@ -1,11 +1,16 @@
 export const RoutesArray: Routes[] = [
-  { title: 'Home', route: 'home', icon: '' },
-  { title: 'Apis', route: 'apis', icon: '', childs: [] },
-  { title: 'Carrinho', route: 'carrinho', icon: '', childs: [] },
-  { title: 'Catálogo', route: 'catalogo', icon: '', childs: [] },
+  { title: 'Home', route: 'home', icon: 'bi bi-house' },
+  { title: 'Apis', route: 'apis', icon: 'bi bi-gear', childs: [] },
+  { title: 'Carrinho', route: 'carrinho', icon: 'bi bi-cart', childs: [] },
+  {
+    title: 'Catálogo',
+    route: 'catalogo',
+    icon: 'bi bi-journal-text',
+    childs: [],
+  },
   { title: 'Entidades', route: 'entidades', icon: '', childs: [] },
-  { title: 'Produtos', route: 'produtos', icon: '', childs: [] },
-  { title: 'Usuários', route: 'usuarios', icon: '', childs: [] },
+  { title: 'Produtos', route: 'produtos', icon: 'bi bi-basket', childs: [] },
+  { title: 'Usuários', route: 'usuarios', icon: 'bi bi-people', childs: [] },
 ];
 
 export class Routes {
