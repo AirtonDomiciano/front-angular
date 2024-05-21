b122617ca1efe8e696b7fc3bf809257f05c8f598
Yuri Schenkel Dalle Teze <106547579+YuriSchenkel@users.noreply.github.com>
2024-05-21 15:44:51 -0300

    subindo ajustes no input-senha e registro

--- src/app/pages/registro-usuario/model/registro-usuario.model.ts
+++ src/app/pages/registro-usuario/model/registro-usuario.model.ts
@@ -3,11 +3,13 @@ export default class RegistroUsuarioModel {
   public sobrenome: string;
   public email: string;
   public senha: string;
+  public confirmarSenha: string;
 
   constructor() {
     this.nome = '';
     this.sobrenome = '';
     this.email = '';
     this.senha = '';
+    this.confirmarSenha = '';
   }
 }
---
+++ src/app/shared/components/input-senha/consts/icons.const.ts
@@ -0,0 +1,1 @@
+export const Icons: string[] = ['bi bi-eye-fill', 'bi bi-eye-slash-fill'];
