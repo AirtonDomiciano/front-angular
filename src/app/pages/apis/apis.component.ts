d48e3291eda2af8ef39d2e76b56dd6617ca71d73
PedroPetrikoski <pedro.ribeiro@xpert.com.br>
2024-05-22 15:04:17 -0300

    subindo ultimas mudanças listagem apis

--- src/app/pages/apis/apis.component.ts
+++ src/app/pages/apis/apis.component.ts
@@ -16,15 +16,8 @@ export class ApisComponent implements OnInit {
 
   constructor(private router: Router, public apisService: ApisService) {}
 
-<<<<<<< HEAD
-  ngOnInit(): void {
-    this.listagemApis = apisMock;
-    console.log(this.listagemApis);
-    this.inicializandoAtivos();
-=======
   async ngOnInit(): Promise<void> {
     this.buscarTodasApis();
->>>>>>> 3b15f55 (subindo-listagem-apis)
   }
 
   async buscarTodasApis() {
@@ -38,29 +31,6 @@ export class ApisComponent implements OnInit {
     this.listaApis = apis;
   }
 
-<<<<<<< HEAD
-  alterouAtivo(id: number) {
-    const ativo = this.formGroup.get(id.toString())?.value;
-    const index = this.listagemApis.findIndex((el) => el.idApi === id);
-    this.listagemApis[index].ativo = ativo;
-    apisMock[index].ativo = this.listagemApis[index].ativo;
-    console.log(apisMock);
-  }
-
-  inicializandoAtivos(): void {
-    const group: any = {};
-    this.listagemApis.forEach((api) => {
-      group[api.idApi!.toString()] = new FormControl(api.ativo === true);
-    });
-    this.formGroup = new FormGroup(group);
-  }
-
-  excluirApi(id: number) {
-    const index = this.listagemApis.findIndex((el) => el.idApi === id);
-    if (index !== -1) {
-      apisMock.splice(index, 1);
-    }
-=======
   adicionarApi() {
     this.router.navigate([`/private/api`]);
   }
@@ -68,7 +38,6 @@ export class ApisComponent implements OnInit {
   excluirApi(id: number) {
     this.apisService.deletarApi(id);
     window.location.reload();
->>>>>>> 3b15f55 (subindo-listagem-apis)
   }
 
   editarApi(id: number) {
--- src/app/pages/apis/model/apis.model.ts
+++ src/app/pages/apis/model/apis.model.ts
@@ -1,9 +1,5 @@
 export class ApisModel {
-<<<<<<< HEAD
-  idApi?: number;
-=======
   idApis?: number;
->>>>>>> 3b15f55 (subindo-listagem-apis)
   nome: string;
   url: string;
   rapidApiHost: string;
