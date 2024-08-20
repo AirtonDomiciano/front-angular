47c059f9b85972bead16b466b7e45575721046a7
PedroPetrikoski <pedro.ribeiro@xpert.com.br>
2024-08-20 13:44:42 -0300

    subindo-botao-voltar

--- src/app/pages/atendimento/atendimento.module.ts
+++ src/app/pages/atendimento/atendimento.module.ts
@@ -8,8 +8,11 @@ import { MultiSelectModule } from 'primeng/multiselect';
 import { MultiSelectProdutosModule } from 'src/app/shared/components/multi-select-produtos/multi-select-produtos.module';
 import { InputDataModule } from '../../shared/components/input-data/input-data.module';
 import { InputHorarioModule } from 'src/app/shared/components/input-horario/input-horario.module';
+<<<<<<< HEAD
 import { TabViewModule } from 'primeng/tabview';
 import { ServicoModule } from '../servico/servico.module';
+=======
+>>>>>>> d0077d8 (subindo-botao-voltar)
 import { BotaoVoltarModule } from 'src/app/shared/components/botao-voltar/botao-voltar.module';
 
 @NgModule({
@@ -24,8 +27,11 @@ import { BotaoVoltarModule } from 'src/app/shared/components/botao-voltar/botao-
     MultiSelectProdutosModule,
     InputDataModule,
     InputHorarioModule,
+<<<<<<< HEAD
     TabViewModule,
     ServicoModule,
+=======
+>>>>>>> d0077d8 (subindo-botao-voltar)
     BotaoVoltarModule,
   ],
   exports: [AtendimentoComponent],
