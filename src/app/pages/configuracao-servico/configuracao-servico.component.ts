e676ff14bbd1821a41672a68a3fb6997a4613682
caua467 <cauakellysilvaesilva@gmail.com>
2024-10-02 16:22:11 -0300

    aperfeiçoando detalhes das configurações

--- src/app/pages/configuracao-servico/configuracao-servico.component.ts
+++ src/app/pages/configuracao-servico/configuracao-servico.component.ts
@@ -5,6 +5,10 @@ import { ProdutosServicoService } from 'src/app/services/produtos-servico.servic
 import ProdutosServicoModel from './model/produtos-servico.model';
 import { UtilsService } from 'src/app/shared/utils/utils.service';
 import { Router } from '@angular/router';
+import { ToastMessageService } from 'src/app/shared/services/toast-message.service';
+import ProdutosServico from 'src/app/shared/model/produtos-servico';
+import { ProdutosService } from 'src/app/services/produtos.service';
+import Produto from 'src/app/shared/model/produtos';
 
 @Component({
   selector: 'app-configuracao-servico',
@@ -16,12 +20,16 @@ export class ConfiguracaoServicoComponent implements OnInit {
 
   public form!: FormGroup;
   public model: ConfiguracaoServicoModel = new ConfiguracaoServicoModel();
+  public produtosSelecionados: Produto[] = [];
+  public servicoSelecionado: number = 0;
 
   constructor(
     private fb: FormBuilder,
     private produtosServicoService: ProdutosServicoService,
     private utils: UtilsService,
-    private router: Router
+    private router: Router,
+    private toast: ToastMessageService,
+    private produtosService: ProdutosService
   ) {
     this.form = this.fb.group(this.model);
   }
@@ -32,10 +40,16 @@ export class ConfiguracaoServicoComponent implements OnInit {
 
   async onSubmit(): Promise<void> {
     if (this.form.invalid) {
+      this.toast.mostrarAviso('É preciso preencher todos os campos.');
       return;
     }
     const input: ConfiguracaoServicoModel = this.form.value;
     const obj: ProdutosServicoModel[] = [];
+
+    if (input.tipoServico.idTipoServico === 0) {
+      this.toast.mostrarAviso('É preciso preencher todos os campos.');
+      return;
+    }
 
     await this.produtosServicoService.deletarPorIdTipoServico(
       input.tipoServico.idTipoServico
@@ -52,11 +66,30 @@ export class ConfiguracaoServicoComponent implements OnInit {
     console.log(res);
 
     this.emitterToast.emit(res);
+    this.toast.mostrarSucesso('Configuração concluída!');
     this.router.navigate([`private/atendimento`]);
   }
 
   setarCamposObrigatorios() {
     const campo: Array<string> = ['tipoServico', 'produtos'];
     this.utils.setarCamposRequeridos(campo, this.form);
+  }
+
+  async setarProdutos(produtosServico: ProdutosServico[]) {
+    const produtos = await this.produtosService.BuscarTodosProdutos();
+
+    for (let i of produtosServico) {
+      const salvar = produtos.find((el) => el.idProdutos === i.idProdutos);
+
+      if (salvar) {
+        this.produtosSelecionados.push(salvar);
+      }
+
+      this.form.controls['produtos'].setValue(this.produtosSelecionados);
+    }
+  }
+
+  setarServicoSelecionado(id: number) {
+    this.servicoSelecionado = id;
   }
 }
